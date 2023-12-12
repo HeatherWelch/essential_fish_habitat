8cf204cdb9bb12be03761c737b6fbf7377eb84a3
HeatherWelch <heather.welch@noaa.gov>
2023-12-12 11:19:16 -0800

    push

--- heather_working/Untitled.R
+++ heather_working/Untitled.R
@@ -10,3 +10,19 @@ credentials::set_github_pat("PAT")
 usethis::use_git_config(user.name = "HeatherWelch", user.email = "heather.welch@noaa.gov")
 usethis::create_github_token() 
 credentials::set_github_pat("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")
+
+
+#####
+library(usethis)
+library(gitcreds)
+gitcreds::gitcreds_set("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")
+gitcreds::gitcreds_get()
+gh::gh_whoami()
+usethis::git_sitrep()
+usethis::gh_token_help()
+
+
+####
+gitcreds::gitcreds_set()
+create_github_token()
+usethis::gh_token_help() 
