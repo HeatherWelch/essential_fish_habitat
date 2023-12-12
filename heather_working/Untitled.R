df7bcbe3547232301a98659d5cd7b9c7b960f356
HeatherWelch <heather.welch@noaa.gov>
2023-12-12 10:25:15 -0800

    does this work?

--- heather_working/Untitled.R
+++ heather_working/Untitled.R
@@ -1,1 +1,12 @@
 test
+
+gitcreds_set()
+PAT="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"
+gitcreds_get()
+
+credentials::set_github_pat("PAT")
+
+
+usethis::use_git_config(user.name = "HeatherWelch", user.email = "heather.welch@noaa.gov")
+usethis::create_github_token() 
+credentials::set_github_pat("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")
